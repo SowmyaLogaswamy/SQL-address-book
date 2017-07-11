f8753c51be0669ed14fc5f16e997928eb47b91d6
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-07-11 15:46:29 -0700

    Test methods like equal() and save()

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,26 +1,58 @@
-import java.util.ArrayList;
 import java.util.List;
+import org.sql2o.*;
 
 public class Entry {
-  String personName;
-  int personPhoneNumber;
-  String personAddress;
+  private String name;
+  private int phone_number;
+  private String address;
+  private int id;
 
 public Entry(String name, int phoneNumber, String address)
 {
-  personName = name;
-  personPhoneNumber = phoneNumber;
-  personAddress = address;
+  this.name = name;
+  this.phone_number = phoneNumber;
+  this.address = address;
 }
 
 public String getName() {
-   return personName;
+   return name;
 }
 public int getPhoneNumber() {
-  return personPhoneNumber;
+  return phone_number;
 }
 public String getAddress() {
-  return personAddress;
+  return address;
+}
+// public static Entry find(int id) {
+//
+// }
+public static List<Entry> all() {
+  String sql = "SELECT * FROM entries";
+  try(Connection con = DB.sql2o.open()) {
+    return con.createQuery(sql).executeAndFetch(Entry.class);
+  }
+}
+
+@Override
+public boolean equals(Object otherEntry) {
+  if(!(otherEntry instanceof Entry)) {
+    return false;
+  } else {
+    Entry newEntry = (Entry) otherEntry;
+    return this.getName().equals(newEntry.getName());
+  }
+}
+
+public void save() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO entries(name, phone_number, address) VALUES (:name, :phoneNumber, :address)";
+    this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("phoneNumber", this.phone_number)
+      .addParameter("address", this.address)
+      .executeUpdate()
+      .getKey();
+  }
 }
 
 }
--- src/test/java/EntryTest.java
+++ src/test/java/EntryTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class EntryTest {
   @Before
   public void setUp() {
-    DB.sql2o. new Sql2o("jdbs:postgresql://localhost:5432/address_book_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/address_book_test", null, null);
   }
 
   // @After
@@ -17,9 +17,59 @@ public class EntryTest {
   // }
 
   @Test
-  public void Entry_instantiatesWithValuesCorrectly_true() {
-    Entry myEntry = new Entry("Kumar", 435, "Redmond");
+  public void Entry_instantiatesValuesCorrectly_true() {
+    Entry myEntry = new Entry("Ally", 435, "Redmond");
     assertEquals(true, myEntry instanceof Entry);
   }
+
+  @Test
+  public void getName_instantiatesWithName_Betty() {
+    Entry myEntry = new Entry("Betty", 638, "Kirkland");
+    assertEquals("Betty", myEntry.getName());
+  }
+
+  @Test
+  public void getPhoneNumber_instantiateWithPhoneNumber_546() {
+    Entry myEntry = new Entry("Anya", 546, "Bellevue");
+    assertEquals(546, myEntry.getPhoneNumber());
+  }
+
+  @Test
+  public void getAddress_instantiatesWithAddress_Seattle() {
+    Entry myEntry = new Entry("Clara", 323, "Seattle");
+    assertEquals("Seattle", myEntry.getAddress());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfEntry_true() {
+    Entry myEntry1 = new Entry("Sowmya", 434, "Redmond");
+    myEntry1.save();
+    Entry myEntry2 = new Entry("Emily", 434, "Redmond");
+    myEntry2.save();
+    assertEquals(true, Entry.all().get(0).equals(myEntry1));
+    assertEquals(true, Entry.all().get(1).equals(myEntry2));
+  }
+
+  @Test
+  public void save_returnsTrueIfNamesAreSame() {
+    Entry myEntry = new Entry("Sowmya", 434, "Redmond");
+    myEntry.save();
+    assertTrue(Entry.all().get(0).equals(myEntry));
+  }
+
+  @Test
+  public void equals_returnsTrueIfNamesAreSame() {
+    Entry myEntry1 = new Entry("Sowmya3", 434, "Redmond");
+    Entry myEntry2 = new Entry("Sowmya3", 434, "Redmond");
+    assertTrue(myEntry1.equals(myEntry2));
+  }
+  @Test
+  public void save_assignsIdToObject() {
+    Entry myEntry = new Entry("Sowmya", 434, "Redmond");
+    myEntry.save();
+    Entry savedEntry = Entry.all().get(0);
+    assertEquals(myEntry.getId(), savedEntry.getId());
+  }
+  @Test
 
 }
